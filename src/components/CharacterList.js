19cdb6d852fac177f965dd67dde57351a77d1818
William <william@monstertipp.com>
2019-02-07 00:08:05 +0100

    [Components] init Characters + CharacterList

---
+++ src/components/CharacterList.js
@@ -0,0 +1,13 @@
+const characterList = {
+  characters: [
+    {
+      id: 1,
+      name: 'Kirby'
+    },
+    {
+      id: 2,
+      name: 'Donkey Kong'
+    }
+  ]
+};
+export default characterList;
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// import characterList from './CharacterList';
 
-export const Characters = () => {
-  return <div>yo</div>;
+// eslint-disable no-console
+
+const imageFolder = `${window.location.origin}/images/character_icons`;
+
+function imigify(name) {
+  const title = name
+    .split(' ')
+    .map(item => item.toLowerCase())
+    .join('');
+  return `${imageFolder}/${title}.png`;
+}
+
+const Character = props => {
+  const { character } = props;
+  return (
+    <div className="character" id={character.id}>
+      <h3>{character.name}</h3>
+      <img src={imigify(character.name)} alt="xxxx" />
+    </div>
+  );
 };
 
+const Characters = props => {
+  const { characters } = props;
+  return (
+    <div className="characters">
+      {characters.map(character => (
+        <Character key={character.id} character={character} />
+      ))}
+    </div>
+  );
+};
 export default Characters;
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+Character.propTypes = {
+  character: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string
+  }).isRequired
+};

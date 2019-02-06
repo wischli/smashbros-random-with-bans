f4885b78ee00ce051a576f3cc3d1b6ad25ce80ca
William <william@monstertipp.com>
2019-02-07 00:07:39 +0100

    [Styling] remove default styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import Characters from './components/Characters';
+import characterList from './components/CharacterList';
 
-export const App = () => {
-  return <div className="App">wqdwqdwq dqwd </div>;
-};
+function App() {
+  const [state, setState] = useState({
+    ...characterList
+    // characters: [
+    //   {
+    //     id: 1,
+    //     name: 'Kirby',
+    //     image: 'kirby.png'
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Donkey Kong',
+    //     image: 'donkeykong.png'
+    //   }
+    // ]
+  });
+  const testState = () => {
+    return {
+      characters: [
+        ...state.characters,
+        {
+          id: 1,
+          name: 'Kirby',
+          image: 'kirby.png'
+        }
+      ]
+    };
+  };
+  return (
+    <div>
+      <Characters characters={state.characters} />
+      <button type="button" onClick={() => setState(testState)}>
+        BUTTON
+      </button>
+    </div>
+  );
+}
+
+// class App extends React.Component {
+//   // constructor() {
+//   //   super();
+//   //   this.state = {
+//   //     characters: [
+//   //       {
+//   //         id: 1,
+//   //         name: 'Kirby',
+//   //         image: 'kirby.png'
+//   //       },
+//   //       {
+//   //         id: 2,
+//   //         name: 'Donkey Kong',
+//   //         image: 'donkeykong.png'
+//   //       }
+//   //     ]
+//   //   };
+//   // }
+//
+//   render() {
+//     return (
+//       <div>
+//         <Characters characters={characters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;

13e5bddc7440351c36d7ae69d304d244704cab50
William <william@monstertipp.com>
2019-03-04 08:59:50 +0100

    [Functionality] add Cookies + handle functions

--- src/App.js
+++ src/App.js
@@ -2,145 +2,97 @@ import React, { useState } from 'react';
 import Characters from './components/Characters';
 import CharacterList from './components/CharacterList';
 import Context from './components/Context';
+import Options from './components/Options';
+import themeStyle from './components/ThemeStyle';
+import { randomize, splitArray, unifyToArray } from './components/Utility';
 
 // const changeCharacters = (chars) => {
 //   return characters.slice(0,1);
 // }
 // export default changeCharacters;
-const splitArray = arrayInput => {
-  const played = [];
-  const disabled = [];
-  const enabled = arrayInput.filter(el => {
-    if (el.played){
-      played.push(el);
-    }
-    else if (!el.enabled) {
-      disabled.push(el);
-    }
-    return !el.played && el.enabled;
-  });
-  return {enabled, played, disabled};
-}
-const unifyToArray = (obj) => {
-  // console.table([...Object.values(obj)]);
-  let array = [];
-  Object.values(obj).map(x => {
-    array = [...array, ...x];
-    return '';
-  });
-  return array;
-}
-
-const randomize = arrayInput => {
-  const {played, enabled, disabled} = splitArray(arrayInput);
-  let currentIndex = enabled.length;
-  let randomIndex;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    [enabled[randomIndex], enabled[currentIndex]] = [enabled[currentIndex], enabled[randomIndex]];
-  }
-  return(unifyToArray({played, enabled, disabled}));
-  // return [...enabled, ...played, ...disabled];
+const initialOptions = {
+  echo: true,
 };
 
 const App = () => {
-  // const [characters, changeCharacters] = useState(CharacterList.slice(0, 5));
   const [characters, changeCharacters] = useState([...CharacterList]);
+  const [options, setOptions] = useState(initialOptions)
 
-  const testState = (oldChars, id = -1) => {
+  const characterEnable = (oldChars, id = -1) => {
     return oldChars.map(char => {
-      // char.enabled = char.id === id ? !char.enabled : char.enabled;
       return char.id === id ? { ...char, enabled: !char.enabled } : { ...char };
     });
   };
-  const handleClick = id => changeCharacters(testState(characters, id));
+  const handleCharClick = id => changeCharacters(characterEnable(characters, id));
+  const handleEchoClick = () => {
+    changeCharacters(characters.map(char => {
+      return {...char, display: char.echo.length  && char.echo[0] % 1 !== 0 ? !char.display : char.display}
+    }))
+    setOptions({...options, echo: !options.echo});
+  }
+  const handleCookieLoad = (chars) => changeCharacters(chars);
   const roundPlayed = inputChars => {
+    // check if all characters have been played or banned => reset
     if (inputChars.filter(char => char.played || !char.enabled).length === inputChars.length - 1) {
       setTimeout(() => {
-        alert('Done Bro, resetting...');
+        console.log('Done Bro, resetting...'); // eslint-disable no-console
         changeCharacters(
           CharacterList.map(character => {
-            const char = character;
-            char.enabled = true;
-            char.played = false;
-            return char;
+            return { ...character, enabled: true, played: false };
           })
         );
       }, 1000);
-      // return [...CharacterList];
     }
     const chars = [...inputChars];
-    const char = chars[0];
-    char.played = !char.played;
-    chars.shift();
+    const char = {
+      ...chars.shift(),
+      played: true
+    };
+    // char.played = !char.played;
+    // chars.shift();
     chars.push(char);
     return unifyToArray(splitArray(chars));
   };
-  const buttonStyle = {
-    width: '50%',
-    height: '50px',
-    margin: 'auto',
-    display: 'inline-flex',
-    textAlign: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    backgroundColor: 'black',
-    color: 'white',
-    borderRadius: '5px',
-    boxShadow: '0px 0px 3px 1px',
-    textTransform: 'uppercase',
-    letterSpacing: '1px',
-    position: window.innerWidth < 401 ? 'fixed' : '',
-    bottom: window.innerWidth < 401 ? '0' : '',
+  const navStyle = {
+    width: 'calc(100% - 20px)',
+    padding: '10px',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    backgroundColor: '#466370',
+    position: window.innerWidth < 1000 ? 'sticky' : '',
+    bottom: window.innerWidth < 1000 ? '0' : ''
+  };
+  const buttonStyle = ({ left }) => {
+    return {
+      width: '49%',
+      backgroundColor: left ? '#ffb3b3' : '#99f9ae',
+      color: left ? '#383838' : 'black',
+      borderColor: left ? '#383838' : 'black',
+    };
   };
   return (
-    <Context.Provider value={{ characters, handleClick }}>
+    <Context.Provider value={{ characters, handleCharClick, themeStyle, handleEchoClick, handleCookieLoad }}>
       <Characters />
-      <button
-        type="button"
-        style={buttonStyle}
-        onClick={() => changeCharacters(randomize(characters))}
-      >
-        Randomize Chars
-      </button>
-      <button
-        type="button"
-        style={buttonStyle}
-        onClick={() => changeCharacters(roundPlayed(characters))}
-      >
-        Set Played Round
-      </button>
+      <Options />
+      <div className="nav" style={navStyle}>
+        <button
+          type="button"
+          style={{...themeStyle.button, ...buttonStyle({left: true})}}
+          onClick={() => changeCharacters(randomize(characters))}
+        >
+          Randomize
+        </button>
+        <button
+          type="button"
+          style={{...themeStyle.button, ...buttonStyle({left: false})}}
+          onClick={() => changeCharacters(roundPlayed(characters))}
+        >
+          Round completed
+        </button>
+      </div>
     </Context.Provider>
   );
 };
-
-// class App extends React.Component {
-//   // constructor() {
-//   //   super();
-//   //   this.state = {
-//   //     characters: [
-//   //       {
-//   //         id: 1,
-//   //         name: 'Kirby',
-//   //         image: 'kirby.png'
-//   //       },
-//   //       {
-//   //         id: 2,
-//   //         name: 'Donkey Kong',
-//   //         image: 'donkeykong.png'
-//   //       }
-//   //     ]
-//   //   };
-//   // }
-//
-//   render() {
-//     return (
-//       <div>
-//         <Characters characters={characters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
---
+++ src/components/Options.js
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react';
+import Cookies from 'universal-cookie';
+import Context from './Context';
+import { getCharactersByIds } from './Utility';
+import CharacterList from './CharacterList';
+
+const Options = () => {
+  const { handleEchoClick, handleCookieLoad, themeStyle, characters } = useContext(Context);
+  const cookies = new Cookies();
+  const buttonStyle = {
+    width: 'calc(100% - 10px)',
+    margin: '10px 5px',
+  }
+  const saveCharacters = (chars) => {
+    const currentState = chars.reduce((obj, char) => {
+      if (char.played){
+        obj.played.push(char.id);
+      }
+      else if (char.enabled){
+        obj.enabled.push(char.id);
+      }
+      else {
+        obj.disabled.push(char.id);
+      }
+      return obj;
+    }, {enabled: [], played: [], disabled: []});
+    cookies.set('characters', JSON.stringify(currentState), { path: '/' });
+  }
+  const loadCharacters = () => {
+    const cookieChars = cookies.get('characters');
+    if (cookieChars){
+      const {enabled, played, disabled} = cookieChars;
+      if (enabled.length + played.length + disabled.length === CharacterList.length){
+        handleCookieLoad(getCharactersByIds({characters: CharacterList, enabled, played, disabled}));
+      }
+    }
+    else {
+      alert('No character save (cookie) found.');
+    }
+  }
+  return (
+    <div style={themeStyle}>
+      <button type="button" style={{...themeStyle.button, ...buttonStyle}} onClick={() => handleEchoClick()}>
+        Echo Click
+      </button>
+      <button type="button" style={{...themeStyle.button, ...buttonStyle}} onClick={() => saveCharacters(characters)}>
+        Save Characters as Cookie
+      </button>
+      <button type="button" style={{...themeStyle.button, ...buttonStyle}} onClick={() => loadCharacters()}>
+        Load Characters from Cookie
+      </button>
+    </div>
+  );
+};
+export default Options;

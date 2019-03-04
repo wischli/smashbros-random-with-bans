862fa4ecf4ac195fdea18c8105848a6eb553b8ef
William <william@monstertipp.com>
2019-03-04 08:49:56 +0100

    [Styling] add ThemeStyle component

---
+++ src/components/ThemeStyle.js
@@ -0,0 +1,28 @@
+const themeStyle = {
+  backgroundColor: '#486471',
+  button: {
+    height: '50px',
+    fontSize: '15px',
+    fontWeight: '700',
+    textAlign: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    borderRadius: '5px',
+    textTransform: 'uppercase',
+    letterSpacing: '1px',
+    border: '2px solid'
+  },
+  // buttonStickyLeft: {
+  //   width: '49%',
+  //   backgroundColor: '#ffb3b3',
+  //   color: '#383838',
+  //   borderColor: '#383838',
+  // },
+  // buttonStickyRight: {
+  //   width: '49%',
+  //   backgroundColor: '#99f9ae',
+  //   color: 'black',
+  //   borderColor: 'black',
+  // }
+};
+export default themeStyle;

9155830ebf53ae44bc454db3c2d39d9635af4f47
William <william@monstertipp.com>
2019-05-01 10:18:16 +0200

    [Styling] Improve Options display

--- src/components/Options.js
+++ src/components/Options.js
@@ -9,44 +9,58 @@ const Options = () => {
   const cookies = new Cookies();
   const buttonStyle = {
     width: 'calc(100% - 10px)',
-    margin: '10px 5px',
-  }
-  const saveCharacters = (chars) => {
-    const currentState = chars.reduce((obj, char) => {
-      if (char.played){
-        obj.played.push(char.id);
-      }
-      else if (char.enabled){
-        obj.enabled.push(char.id);
-      }
-      else {
-        obj.disabled.push(char.id);
-      }
-      return obj;
-    }, {enabled: [], played: [], disabled: []});
+    margin: '4px 5px'
+  };
+  const saveCharacters = chars => {
+    const currentState = chars.reduce(
+      (obj, char) => {
+        if (char.played) {
+          obj.played.push(char.id);
+        } else if (char.enabled) {
+          obj.enabled.push(char.id);
+        } else {
+          obj.disabled.push(char.id);
+        }
+        return obj;
+      },
+      { enabled: [], played: [], disabled: [] }
+    );
     cookies.set('characters', JSON.stringify(currentState), { path: '/' });
-  }
+  };
   const loadCharacters = () => {
     const cookieChars = cookies.get('characters');
-    if (cookieChars){
-      const {enabled, played, disabled} = cookieChars;
-      if (enabled.length + played.length + disabled.length === CharacterList.length){
-        handleCookieLoad(getCharactersByIds({characters: CharacterList, enabled, played, disabled}));
+    if (cookieChars) {
+      const { enabled, played, disabled } = cookieChars;
+      if (enabled.length + played.length + disabled.length === CharacterList.length) {
+        handleCookieLoad(
+          getCharactersByIds({ characters: CharacterList, enabled, played, disabled })
+        );
       }
+    } else {
+      console.log('No character save (cookie) found.');
     }
-    else {
-      alert('No character save (cookie) found.');
-    }
-  }
+  };
   return (
-    <div style={themeStyle}>
-      <button type="button" style={{...themeStyle.button, ...buttonStyle}} onClick={() => handleEchoClick()}>
+    <div style={{ ...themeStyle, paddingBottom: 80 }}>
+      <button
+        type="button"
+        style={{ ...themeStyle.button, ...buttonStyle }}
+        onClick={() => handleEchoClick()}
+      >
         Echo Click
       </button>
-      <button type="button" style={{...themeStyle.button, ...buttonStyle}} onClick={() => saveCharacters(characters)}>
+      <button
+        type="button"
+        style={{ ...themeStyle.button, ...buttonStyle }}
+        onClick={() => saveCharacters(characters)}
+      >
         Save Characters as Cookie
       </button>
-      <button type="button" style={{...themeStyle.button, ...buttonStyle}} onClick={() => loadCharacters()}>
+      <button
+        type="button"
+        style={{ ...themeStyle.button, ...buttonStyle }}
+        onClick={() => loadCharacters()}
+      >
         Load Characters from Cookie
       </button>
     </div>

dfc3dd3c4c1a31819de65c921db1530fc4bf5925
William <william@monstertipp.com>
2019-08-11 23:27:01 +0200

    [Tests - Character] Add enzyme shallow render tests

---
+++ src/components/Characters/Character/Character.test.tsx
@@ -0,0 +1,85 @@
+/* eslint-disable no-undef */
+
+import { configure, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { Character } from './Character-view';
+import { color } from '../../../layout/themeStyle';
+import { charArr } from '../../../model/charArr/charArr';
+import { IChar } from '../../../types/Types';
+
+configure({ adapter: new Adapter() });
+
+// set default test data
+const context = {
+	handleCharClick: jest.fn()
+};
+let testChar: IChar;
+beforeEach(() => {
+	testChar = charArr[0];
+});
+
+describe('Testing Character Component', () => {
+	it('Checks enabled character', () => {
+		const wrapper = shallow(
+			<Character
+				character={testChar}
+				stateKey="enabled"
+				charIndex={0}
+				handleCharClick={context.handleCharClick}
+			/>
+		);
+		try {
+			expect(wrapper.get(0).props.children.props.style.opacity).toEqual(1);
+			expect(wrapper.get(0).props.style.backgroundColor).toBe(color.charEnabled);
+		} catch (err) {
+			expect(err).toBeUndefined();
+		}
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Checks disabled character', () => {
+		const wrapper = shallow(
+			<Character
+				character={testChar}
+				stateKey="disabled"
+				charIndex={0}
+				handleCharClick={context.handleCharClick}
+			/>
+		);
+		try {
+			expect(wrapper.get(0).props.children.props.style.opacity).toEqual(0.6);
+			expect(wrapper.get(0).props.style.backgroundColor).toBe(color.charDisabled);
+		} catch (err) {
+			expect(err).toBeUndefined();
+		}
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Checks played character', () => {
+		const wrapper = shallow(
+			<Character character={testChar} stateKey="played" charIndex={0} handleCharClick={context.handleCharClick} />
+		);
+		try {
+			expect(wrapper.get(0).props.children.props.style.opacity).toEqual(0.6);
+			expect(wrapper.get(0).props.style.backgroundColor).toBe(color.charPlayed);
+		} catch (err) {
+			expect(err).toBeUndefined();
+		}
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('Checks hidden character', () => {
+		const wrapper = shallow(
+			<Character character={testChar} stateKey="hidden" charIndex={0} handleCharClick={context.handleCharClick} />
+		);
+		try {
+			expect(wrapper.get(0).props.children.props.style.opacity).toEqual(0.6);
+			expect(wrapper.get(0).props.style.backgroundColor).toBe(color.charHidden);
+		} catch (err) {
+			expect(err).toBeUndefined();
+		}
+		expect(wrapper).toMatchSnapshot();
+	});
+});

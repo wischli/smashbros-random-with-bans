670adde30ee2971c3f90caba4349240c962a72fb
William <william@monstertipp.com>
2019-08-11 23:25:34 +0200

    [Components] Rename charArray to charArr

--- src/model/charArr/charArr.ts
+++ src/model/charArr/charArr.ts
@@ -2,7 +2,7 @@ import { IChar, IState } from '../../types/Types';
 import { characters } from './characters';
 import { imigify } from '../../utils';
 
-export const charArray: IChar[] = characters.map(char => 
+export const charArr: IChar[] = characters.map(char => 
   ({
     ...char,
     media: imigify(char.name),
@@ -12,4 +12,4 @@ export const charArray: IChar[] = characters.map(char =>
   })
 );
 
-export const initialCharState: IState = { enabled: charArray.map((char, index) => index), played: [], disabled: [], hidden: [] };
+export const initialCharState: IState = { enabled: charArr.map((char, index) => index), played: [], disabled: [], hidden: [] };

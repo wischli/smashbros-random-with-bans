da054ea24db3348879923ee2c0290622f9509e20
William Freudenberger <w.wischlinsky@me.com>
2020-05-14 21:16:25 +0200

    Add DLC chars (#26)
    * fix: error throw for char index 0
    
    * chore: update dependencies
    
    * refactor: improve code styling
    
    * feat: add dlc characters

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,41 +1,41 @@
-import Cookies from "universal-cookie";
-import { IState, ICState } from "../types/Types";
+import Cookies from 'universal-cookie'
+import { IState, ICState } from '../types/Types'
 
 // set image location in dependence of character name
 export const imigify = (name: string) => {
-    const { href: location } = window.location;
-    const root = 'https://wischli.github.io/smashbros-random-with-bans/';
-    const imagePath = 'images/character_icons/';
-    const imageFolder = `${location.includes('local') ? location: root}${imagePath}`;
-    const title = name
-        .split(/[. \-&]/g)
-        .map(item => item.toLowerCase())
-        .join('');
-    return `${imageFolder}/${title}.png`;
+  const { href: location } = window.location
+  const root = 'https://wischli.github.io/smashbros-random-with-bans/'
+  const imagePath = 'images/character_icons'
+  const imageFolder = `${location.includes('local') ? `${location}public/` : root}${imagePath}`
+  const title = name
+    .split(/[. \-&]/g)
+    .map(item => item.toLowerCase())
+    .join('')
+  return `${imageFolder}/${title}.png`
 }
 
 // save current character state in cookie
 export const saveCharacters = (inputState: IState) => {
-    const cookies = new Cookies();
-    const currentState = Object.keys(inputState).reduce(
-        (cookieState: ICState, key: string) => {
-            const theKey = key as keyof IState;
-            return { ...cookieState, [key]: inputState[theKey].map((charIndex: number) => charIndex) };
-        },
-        { enabled: [], played: [], disabled: [], hidden: [], date: new Date().toJSON() },
-    );
-    cookies.set('characters', JSON.stringify(currentState), { path: '/' });
-};
+  const cookies = new Cookies()
+  const currentState = Object.keys(inputState).reduce(
+    (cookieState: ICState, key: string) => {
+      const theKey = key as keyof IState
+      return { ...cookieState, [key]: inputState[theKey].map((charIndex: number) => charIndex) }
+    },
+    { enabled: [], played: [], disabled: [], hidden: [], date: new Date().toJSON() },
+  )
+  cookies.set('characters', JSON.stringify(currentState), { path: '/' })
+}
 
 // randomize array
 export const randomize = (inputArr: number[]) => {
-    const arr = [...inputArr];
-    let currentIndex: number = arr.length;
-    let randomIndex = 0;
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        [arr[randomIndex], arr[currentIndex]] = [arr[currentIndex], arr[randomIndex]];
-    }
-    return arr;
-};
+  const arr = [...inputArr]
+  let currentIndex: number = arr.length
+  let randomIndex = 0
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    ;[arr[randomIndex], arr[currentIndex]] = [arr[currentIndex], arr[randomIndex]]
+  }
+  return arr
+}
